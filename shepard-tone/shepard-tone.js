d6882f77146695775f23f5a8d93924675862944b
Viola <94903755+violangg@users.noreply.github.com>
2021-11-25 20:19:59 +0100

    fa sempre cag

--- shepard-tone/shepard-tone.js
+++ shepard-tone/shepard-tone.js
@@ -1,72 +1,72 @@
-c = new AudioContext()
+var c = new AudioContext();
 
-var attack = 0.01;
-var release = 0.2;
+var g = c.createGain();
+g.connect(c.destination);
 
-function play(note) {
-  const o = c.createOscillator();
-  const g = c.createGain();
-  o.frequency.value = 440*Math.pow(2,note/12);
-  o.connect(g);
-  g.connect(c.destination);
-  const now = c.currentTime;
-  g.gain.setValueAtTime(0, now);
-  g.gain.linearRampToValueAtTime(1, now+attack);
-  g.gain.linearRampToValueAtTime(0, now+attack+release);
-  o.start();
-  o.stop(now+attack+release);
+var one_loop_duration = 5; // seconds
+var step_speed = 1000 * one_loop_duration / 12; 
+
+var current_step = 0;
+var step = [];
+
+var gt = [];
+
+var base = Math.pow(2, 1/12);
+
+function shepardscale () {
+
+    for(i = 0; i < 3; i++) {
+      
+        if(step[i]) {
+            step[i].stop()
+        }
+
+        tone = c.createOscillator();
+
+        var octave = 0;
+        
+        if (i == 1) {
+            octave = 12;
+        }
+        if (i == 2) {
+            octave = 24;
+        }
+
+        freq = 440*Math.pow(base, current_step + octave);
+        tone.frequency.value = freq; 
+
+        gt[i] = c.createGain();
+        gt[i].gain.value = 1;
+
+        if (octave == 0) {
+            gt[i].gain.linearRampToValueAtTime(1, one_loop_duration);
+        }
+        if (octave == 24) {
+            gt[i].gain.linearRampToValueAtTime(0, one_loop_duration);
+        }
+
+        tone.connect(gt[i]);
+
+        gt[i].connect(c.destination); 
+        tone.start();
+
+        step[i] = tone;
+    }
+
+    current_step = (current_step + 1) % 12; // 12 steps in ogni loop
+    setTimeout(shepardscale, step_speed); 
 }
 
-function stop() {
-    o.stop()
+
+setVolume(0); // Initialize volume to match range input
+var playing = false;
+
+function start() {
+    if(!playing) {
+        playing = true;
+        shepardscale();
+    }
 }
-
-function playst() {
-    setTimeout(()=>play(-9),c.currentTime)
-    setTimeout(()=>play(3),c.currentTime)
-    setTimeout(()=>play(-21),c.currentTime)
-
-    setTimeout(()=>play(-8),c.currentTime+300)
-    setTimeout(()=>play(4),c.currentTime+300)
-    setTimeout(()=>play(-20),c.currentTime+300)
-
-    setTimeout(()=>play(-7),c.currentTime+600)
-    setTimeout(()=>play(5),c.currentTime+600)
-    setTimeout(()=>play(-19),c.currentTime+600)
-
-    setTimeout(()=>play(-6),c.currentTime+900)
-    setTimeout(()=>play(6),c.currentTime+900)
-    setTimeout(()=>play(-18),c.currentTime+900)
-
-    setTimeout(()=>play(-5),c.currentTime+1200)
-    setTimeout(()=>play(7),c.currentTime+1200)
-    setTimeout(()=>play(-17),c.currentTime+1200)
-
-    setTimeout(()=>play(-4),c.currentTime+1500)
-    setTimeout(()=>play(8),c.currentTime+1500)
-    setTimeout(()=>play(-16),c.currentTime+1500)
-
-    setTimeout(()=>play(-3),c.currentTime+1800)
-    setTimeout(()=>play(9),c.currentTime+1800)
-    setTimeout(()=>play(-15),c.currentTime+1800)
-
-    setTimeout(()=>play(-2),c.currentTime+2100)
-    setTimeout(()=>play(10),c.currentTime+2100)
-    setTimeout(()=>play(-14),c.currentTime+2100)
-
-    setTimeout(()=>play(-1),c.currentTime+2400)
-    setTimeout(()=>play(11),c.currentTime+2400)
-    setTimeout(()=>play(-13),c.currentTime+2400)
-
-    setTimeout(()=>play(0),c.currentTime+2700)
-    setTimeout(()=>play(12),c.currentTime+2700)
-    setTimeout(()=>play(-12),c.currentTime+2700)
-
-    setTimeout(()=>play(1),c.currentTime+3000)
-    setTimeout(()=>play(13),c.currentTime+3000)
-    setTimeout(()=>play(-11),c.currentTime+3000)
-
-    setTimeout(()=>play(2),c.currentTime+3300)
-    setTimeout(()=>play(14),c.currentTime+3300)
-    setTimeout(()=>play(-10),c.currentTime+3300)
+function setVolume(volume) {
+    g.gain.value = volume / 100 / 12;
 }
