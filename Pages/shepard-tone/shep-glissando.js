3636c9dc4a30f58db6ccf800bb4376065ab06fbe
Viola <94903755+violangg@users.noreply.github.com>
2021-12-10 15:28:22 +0100

    giochino marcio

--- Pages/shepard-tone/shep-glissando.js
+++ Pages/shepard-tone/shep-glissando.js
@@ -12,10 +12,7 @@ var freq = [8000]; // 8000
 var lowestFreq = 50; // 50 
 var highestFreq = 16000; // 16000
 
-function shepardtone() {
-
-    var canvasElement = document.querySelector('#canvas');
-    var ctx = canvasElement.getContext('2d');
+function ascendingShepard() {
 
     for (i = 1; i <= num_osc; i++) {
     osc[i] = c.createOscillator();
@@ -49,22 +46,46 @@ function shepardtone() {
 
         }
 
-        /* var data = new Float32Array(2048);
-        a.getFloatTimeDomainData(data);
-        console.log(data);
+    }, 100); //update hz every 75ms
 
-        function draw() {
-            a.getFloatTimeDomainData(data);
-            ctx.clearRect(0,0,1000,1000);
-            ctx.beginPath();
-            const h = canvasElement.height;
-            ctx.moveTo(0,h/2)
-            for(var i=0;i<2048;i++) {
-                ctx.lineTo(i, h/2+data[i]*h/2)
+    for (i = 1; i <= num_osc; i++) {
+        osc[i].start(); 
+    } 
+}
+
+function descendingShepard() {
+
+    for (i = 1; i <= num_osc; i++) {
+    osc[i] = c.createOscillator();
+    freq[i] = freq[i - 1] / 2; // highest[1] 8000, lowest[10] 7.81
+    osc[i].frequency.value = freq[i]; 
+
+    gain[i] = c.createGain(); 
+    gain[i].gain.value = 0; 
+    }
+
+    play = setInterval(function() {
+
+        for (i = 1; i <= num_osc; i++) {
+
+            if (freq[i] < 20) {
+                freq[i] = 20000;
+            } 
+            else {
+                freq[i] = freq[i] - (freq[i] / 200); // 192
             }
-            ctx.stroke()
+
+            osc[i].frequency.value = freq[i]; 
+
+            if (lowestFreq < freq[i] < highestFreq) {
+                gain[i].gain.value = 0.02; 
+            }
+
+            osc[i].connect(gain[i]);
+            gain[i].connect(a);
+            a.connect(c.destination);
+
         }
-        draw(); */
 
     }, 100); //update hz every 75ms
 
