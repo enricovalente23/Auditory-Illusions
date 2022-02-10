5e8a7924ce1dbf24d8d6c896d882d3c73de5f0ed
Enrico Francesco Valente <enrico.valente23@gmail.com>
2022-02-10 21:52:01 +0100

    enrico FINISHED

---
+++ Pages/illusory-discontinuity/illusory-discontinuity.js
@@ -0,0 +1,28 @@
+
+var context1 = new AudioContext()
+var context2 = new AudioContext()
+
+var frequenza1 = 880;      // LA5 tone frequency
+var frequenza2 = 110;      // LA2 noise frequency
+var durata1 = 5;           // 5 seconds tone
+var durata2 = 0.07;        // noise duration
+
+function suonatono()
+{
+    var oscillator1 = context1.createOscillator()
+    oscillator1.type = "sine"
+    oscillator1.connect(context1.destination)
+    oscillator1.frequency.value = frequenza1
+    oscillator1.start(context1.currentTime)
+    oscillator1.stop(context1.currentTime + durata1)
+}
+
+function suonarumore()
+{
+    var oscillator2 = context2.createOscillator()
+    oscillator2.type = "square"          
+    oscillator2.connect(context2.destination)
+    oscillator2.frequency.value = frequenza2
+    oscillator2.start(context2.currentTime)
+    oscillator2.stop(context2.currentTime + durata2)
+}
